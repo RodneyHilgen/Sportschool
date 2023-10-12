f377552e42dfc2da79130db729c70ffb7c96a8fa
Rodney Hilgen <rodneyhilgen@live.nl>
2023-10-12 09:08:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,4 @@ hamburger.addEventListener("click", () => {
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}))
